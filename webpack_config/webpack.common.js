f70f17f7088515d2fce4a158a94815d275a00d4d
Sydney Barovsky <sbarovsky@gmail.com>
2020-09-12 18:13:22 -0700

    first commit

---
+++ webpack_config/webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const extract = require("mini-css-extract-plugin");
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+    // HTML PARTIALS RULES
+    { test: /\.(html)$/,
+      // include: path.resolve(__dirname, './assets/pages'),
+      use: {
+        loader: 'html-loader',
+        options: {
+          interpolate: true } } },
+    // JAVASCRIPT RULES
+    { test: /\.js$/,
+      exclude: /(node_modules)/,  // ignore node_module JS files
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'] } } },
+    // (S)CSS RULES
+    { test:/\.(sa|sc|c)ss$/,
+      use: [
+        { loader: extract.loader },   // gets all transformed CSS and extracts it into a separate single bundled file
+        { loader: 'css-loader' },     // resolves url() and @imports inside CSS
+        { loader: 'postcss-loader' }, // applies autoprefixer and minifying
+        { loader: 'sass-loader',      // transform SASS to standard CSS
+          options: { implementation: require('sass') }
+        } ]
+    },
+    // IMAGE RULES
+    { test: /\.(pdf|png|jpe?g|gif|svg|ico)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './assets'
+          }
+        } ]
+      },
+    // FONT RULES
+    { test: /\.(woff|woff2|ttf|otf|eot)$/,
+      use: [
+        { loader: 'file-loader',
+          options: { outputPath: './assets' } } ] }
+    ] },
+  plugins: [
+    new extract({
+      filename: 'bundle.css'
+    }),
+    new HTMLWebpackPlugin({
+      title: 'WEBPACK BOILERPLATE',
+      template: './src/index.html',
+      filename: 'index.html',
+      // favicon: "./assets/images/favicon/favicon.ico",
+    }),
+    new dotenv(),
+    new CleanWebpackPlugin()
+  ],
+};
